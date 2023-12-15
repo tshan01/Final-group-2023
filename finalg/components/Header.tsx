c3d4d84a81d65dc11f317a61465cc64f2b570cd3
Shritha <sv963@njit.edu>
2023-12-15 01:19:27 -0500

    24

--- finalg/components/Header.tsx
+++ finalg/components/Header.tsx
@@ -1,10 +1,6 @@
-'use client'
-// Header.js
-
-
 import React from "react";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
-
+import Image from 'next/image'; // Importing Next.js Image component
 
 const Header = () => {
   const openMailchimpSignup = () => {
@@ -18,7 +14,7 @@ const Header = () => {
   return (
     <Navbar className="coffee-bean-color">
       <NavbarBrand>
-        <img src="/TB1.png" alt="Your Brand Logo" style={{ width: '100px', height: '100px' }} /> 
+        <Image src="/TB1.png" alt="Your Brand Logo" width={100} height={100} />
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center" style={{ flexGrow: 1 }}>
         <NavbarItem>
@@ -28,17 +24,16 @@ const Header = () => {
         </NavbarItem>
         <NavbarItem isActive>
           <Link color="foreground" href="/cards">
-          <a>MENU</a>
+            <a>MENU</a>
           </Link>
         </NavbarItem>
         <NavbarItem>
           <Link color="foreground" href="/about">
-           <a>ABOUT</a>
+            <a>ABOUT</a>
           </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        
         <NavbarItem>
           <Button color="primary" onClick={openMailchimpSignup} variant="flat">
             SIGN UP
