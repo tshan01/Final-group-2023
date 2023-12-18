4ba1318e388517064bd50e58722b7abeec2ae9a5
Shritha <sv963@njit.edu>
2023-12-18 03:50:54 -0500

    1st  playwright

---
+++ finalg/tests/example.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+// Define the URL of your Next.js app
+const appURL = 'http://localhost:3000'; // Update with your actual URL
+
+test('Test Hero Component', async ({ page }) => {
+  // Navigate to the page with the Hero component
+  await page.goto(appURL);
+
+  // Wait for the Hero component to be visible
+  await page.waitForSelector('.hero');
+
+  // Perform assertions on the Hero component
+  const heroText = await page.textContent('.hero h1');
+  const imageAltText = await page.getAttribute('.hero img', 'alt');
+
+  // Replace these assertions with the expected content from your Hero component
+  expect(heroText).toContain('Experience India');
+  expect(imageAltText).toBe('Coffee Image');
+});
+
